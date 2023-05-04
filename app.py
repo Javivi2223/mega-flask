edf5debeef70e33716f3f183595da675fc1c2e30
Javiera Guerrero <javiera.guerrero@@soymas.cl>
2023-05-04 15:58:56 -0400

    se agrega formulario

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hola_gentecita():
     return render_template("index.html")
+
+@app.route('/reels')
+def bienvenidos(): 
+    name = request.args.get("username") 
+    lastname = request.args.get("lastname") 
+    return render_template("reels.html", name=name,lastname=lastname)
